dd68c0996746d2615c6b5887431b4de25c045a93
MikkelHansen95 <mikkelhansen95@live.dk>
2017-09-20 17:37:00 +0200

    entiteter

--- src/main/java/entities/Topping.java
+++ src/main/java/entities/Topping.java
@@ -11,4 +11,42 @@ package entities;
  */
 public class Topping {
     
+    int id;
+    String name;
+    double price;
+
+    public Topping(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+    
+    
+    
+    
 }
