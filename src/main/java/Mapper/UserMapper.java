4192ecf8647930f2d8634048978b754ed4b9f1a2
Weiqifan1 <cmlykke@hotmail.com>
2017-09-21 11:08:18 +0200

    added try catch to all mappers

--- src/main/java/Mapper/UserMapper.java
+++ src/main/java/Mapper/UserMapper.java
@@ -51,24 +51,27 @@ public class UserMapper {
     
     public User getUserByID(int id) throws SQLException {
         User output = null;
-        String sql = "SELECT user_id,username, password,balance, email FROM cupcake.users where user_id=" + id;
-        PreparedStatement pstmt = Connector.getConnection().prepareStatement(sql);
-        ResultSet rs = pstmt.executeQuery();
-        int userID = 0;
-        String userName = "";
-        String password = "";
-        double balance = 0;
-        String email = "";
-        while (rs.next()) {
-            userID = id;//rs.getInt("user_id");
-            userName = rs.getString("username");
-            password = rs.getString("password");
-            balance = rs.getDouble("balance");
-            email = rs.getString("email");
+        try{
+            String sql = "SELECT user_id,username, password,balance, email FROM cupcake.users where user_id=" + id;
+            PreparedStatement pstmt = Connector.getConnection().prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            int userID = 0;
+            String userName = "";
+            String password = "";
+            double balance = 0;
+            String email = "";
+            while (rs.next()) {
+                userID = id;//rs.getInt("user_id");
+                userName = rs.getString("username");
+                password = rs.getString("password");
+                balance = rs.getDouble("balance");
+                email = rs.getString("email");
+            }
+            output = new User(userName, password, balance, email);
+            output.setUser_id(id);
+        }catch (Exception e) {
+            return null;
         }
-        output = new User(userName, password, balance, email);
-        output.setUser_id(id);
-        
         return output;
     }
     
